49b7652be1486e794587005ec61276d3a6f2ec70
jerh011 <128959299+jerh011@users.noreply.github.com>
2023-09-28 11:21:03 -0700

    Update MainPage.xaml.cs

--- Triangulo_JERH/Triangulo_JERH/MainPage.xaml.cs
+++ Triangulo_JERH/Triangulo_JERH/MainPage.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -15,62 +15,106 @@ namespace Triangulo_JERH
             InitializeComponent();
         }
 
+        float _numeroGrande;
+        float _sumaDosLados;
+        float _Area;
+        float _Base;
+        float _Lado1;
+        float _Lado2;
+        float _Altura;
+
         private void Area(object sender, EventArgs e)
         {
 
             if (!string.IsNullOrEmpty(Base.Text) && !string.IsNullOrEmpty(Lado_1.Text) && !string.IsNullOrEmpty(Lado_2.Text) && !string.IsNullOrEmpty(Altura.Text))
             {
-                float _Area;
-                float _Base = float.Parse(Base.Text);
-                float _Lado1 = float.Parse(Lado_1.Text);
-                float _Lado2 = float.Parse(Lado_2.Text);
-                float _Altura = float.Parse(Altura.Text);
-               
-               
-                float _numeroGrande;
-                float _sumaDosLados;
-
-                if (_Base > _Lado1 && _Base > _Lado2) {
-                    _numeroGrande = _Base;
-                    _sumaDosLados= _Lado2 - _Lado1;
-
-
-                }
-                else if (_Lado1 > _Lado2 && _Lado1 > _Base) { 
-                     _numeroGrande = _Lado1;
-                    _sumaDosLados = _Base + _Lado2; 
-                }
-                   else { 
-                    _numeroGrande = _Lado2;
-                    _sumaDosLados = _Base + _Lado1;
-                }
-
+                _Base = float.Parse(Base.Text);
+                _Lado1 = float.Parse(Lado_1.Text);
+                _Lado2 = float.Parse(Lado_2.Text);
+                _Altura = float.Parse(Altura.Text);
+                ConfirMarLadoGrande(_Base, _Lado1, _Lado2);
                 if (_numeroGrande < _sumaDosLados)
                 {
-                    _Area = (_Base * _Altura) / 2;
-                    AreaTriangulo.Text = _Area.ToString();
-                    if (_Base != _Lado1 && _Base != _Lado2 && _Lado2 != _Lado1)
-                    {
-                        Tipo.Text = "Escaleno";
-                        imgEquilatero.IsVisible= true;
-                    }
-                    else if (_Base == _Lado1 && _Base == _Lado2 && _Lado2 == _Lado1)
-                    {
-                        Tipo.Text = "Equilatero";
-                    }
-                    else if (_Base == _Lado1 && _Base == _Lado2 && _Lado2 != _Lado1) { 
-                    Tipo.Text = "Isoseles";
-                    }
+                    TipoTriangulo(_sumaDosLados, _numeroGrande);
+                    CalcularArea(_Base, _Altura);
+                    MostrarTriangulo(Tipo.Text);
                 }
-                 else
+                else
                     DisplayAlert("Datos Incorrectos", "No es un triangulo", "Cerrar");
+
             }
             else
-             
-            DisplayAlert("Datos Incorrectos", "ingrese todos los datos", "Cerrar");
+
+                DisplayAlert("Datos Incorrectos", "ingrese todos los datos", "Cerrar");
+
+        }
+
+        public void CalcularArea(float _Base, float _Altura)
+        {
+            _Area = (_Base * _Altura) / 2;
+            AreaTriangulo.Text = _Area.ToString();
+        }
+
+
+        public void ConfirMarLadoGrande(float _Base, float _Lado1, float _Lado2)
+        {
+
+            if (_Base > _Lado1 && _Base > _Lado2)
+            {
+                _numeroGrande = _Base;
+                _sumaDosLados = _Lado2 + _Lado1;
+
+
+            }
+            else if (_Lado1 > _Lado2 && _Lado1 > _Base)
+            {
+                _numeroGrande = _Lado1;
+                _sumaDosLados = _Base + _Lado2;
+            }
+            else
+            {
+                _numeroGrande = _Lado2;
+                _sumaDosLados = _Base + _Lado1;
+            }
+        }
+        public void TipoTriangulo(float _sumaDosLados, float _numeroGrande)
+        {
+
+            if (_Base != _Lado1 && _Base != _Lado2 && _Lado2 != _Lado1)
+                Tipo.Text = "Escaleno";//si lo muestra
+            else if (_Base == _Lado1 && _Base == _Lado2 && _Lado2 == _Lado1)
+                Tipo.Text = "Equilatero";//si lo muestra
+
+            else //if (_Base == _Lado1 && _Base == _Lado2 && _Lado2 != _Lado1)
+                Tipo.Text = "Isoseles";
+
+        }
+
+        public void MostrarTriangulo(string _Tipo)
+        {
+            if (_Tipo == "Escaleno")
+            {
+                imgEquilatero.IsVisible = false;
+                imgEscaleno.IsVisible = true;
+                imgisosceles.IsVisible = false;
+            }
+
+            else if (_Tipo == "Equilatero")
+            {
+                imgEquilatero.IsVisible = true;
+                imgEscaleno.IsVisible = false;
+                imgisosceles.IsVisible = false;
+            }
+            else if (_Tipo == "Isoseles")
+            {
+                imgEquilatero.IsVisible = false;
+                imgEscaleno.IsVisible = false;
+                imgisosceles.IsVisible = true;
+
+            }
+
 
         }
     }
+
 }
-
-
